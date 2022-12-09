effcf54a1a510e3d058c0c9806379b144064c92c
Gustaf Jäderberg <gustaf@jaderb.se>
2022-12-09 13:20:32 +0100

    start

--- script.js
+++ script.js
@@ -25,4 +25,19 @@ function renderBookList(bookList) {
 
   existingElement && root.removeChild(existingElement);
   bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
+
+  const item = document.getElementById("item");
+  item.addEventListener("mouseover", (event) => {
+    event.target.insertAdjacentHTML('beforeend', `<div class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto" id="item"></div>`);
+  }, false);
+
+  item.addEventListener('mouseleave', (event) => {
+    event.target.nextSibling.remove()
+  });
+}
+
+
+
+function renderBookDetails(element) {
+
 }
